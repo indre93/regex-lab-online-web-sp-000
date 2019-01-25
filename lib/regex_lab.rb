0e49f750ee2226eef304445b3499e1ddf4d48e8b
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-25 03:43:29 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(words)
 end
 
 def words_starting_with_un_and_ending_with_ing(words)
-  words.scan(/\^[un]/)
+  words.scan(/\^[un]+$[ing]/)
 end
 
 def words_five_letters_long(words)
